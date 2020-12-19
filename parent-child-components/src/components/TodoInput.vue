568f6cbf862e01fc4ec74d19cf8511aad13e32b7
kaiz16 <realkaiz7@gmail.com>
2020-12-19 19:20:09 +0800

    sfc

---
+++ parent-child-components/src/components/TodoInput.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="child border">
+        <code>Child of Todos.vue</code>
+        <p>TodoInput.vue</p>
+        <code>
+        Add todos here. Hit Enter
+        <input type="text" 
+            v-on:keyup.enter="emitToParent" 
+            v-model="todotitle"/>
+        </code>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            todotitle: null
+        }
+    },
+    methods: {
+        emitToParent(){
+            // Data validation
+            if (!this.todotitle){
+                alert('eh, no falsy value ok ?')
+                
+                return
+            }
+            alert('I will now emit an action to my parent.')
+            
+            // we can also pass in params when emitting to parent
+            this.$emit('add-todo', this.todotitle)
+            this.todotitle = null
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ parent-child-components/src/components/Todos.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="parent child border">
+        <code>Child of App.vue</code>
+        <p>Todos.vue</p>
+        <!-- Catch an event emitted by our todo input -->
+        <TodoInput v-on:add-todo="addTodo"></TodoInput>
+
+        <!-- A key must be a unique 
+        so that vue can identify by their key -->
+        <!-- passing in the singular todo 
+            and unique index as props -->
+        <TodoItem 
+            v-for="(todo, index) in todos" 
+            v-bind:key="index"
+            :index="index + 1"
+            :todo="todo"></TodoItem>
+    </div>
+</template>
+
+<script>
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
+export default {
+    components: {
+        TodoInput,
+        TodoItem
+    },
+    data(){
+        return {
+            todos: ['Walk my dog', 'What comes first chicken or egg ?']
+        }
+    },
+    methods: {
+        // a parameter or a payload passed by child can be used here
+        addTodo(todo){
+            this.$emit('add-todo')
+            // console.log(todo)
+            // adding the new item to our todos array
+            this.todos.unshift(todo)
+        }
+    }
+}
+</script>
