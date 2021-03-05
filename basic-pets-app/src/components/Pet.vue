79f2fdb0a347c81fbdfaf1884f7f31e3a02838cc
kaiz16 <realkaiz7@gmail.com>
2021-03-05 22:49:40 +0800

    add basic-pets-app

---
+++ basic-pets-app/src/components/Pet.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <img v-bind:src="pet.type === 'cat' ? catImg : dogImg"/>
+        <p>{{ pet.name }}</p>
+        <button v-on:click="del()">Delete</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        pet: {
+            type: Object
+        }
+    },
+    data(){
+        return {
+            catImg: 'https://i.pinimg.com/originals/24/37/bd/2437bd9758a684862c11aa9e8f20341a.jpg',
+            dogImg: 'https://i.pinimg.com/originals/79/ff/00/79ff00f7e84d5246a08e74c8d0e8e115.jpg'
+        }
+    },
+    methods: {
+        del(){
+            this.$emit('deletePet')
+        }
+    }
+}
+</script>
+
+<style>
+img{
+    height: 100px;
+    width: 100px;
+}
+</style>
---
+++ basic-pets-app/src/components/Pets.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <Pet v-for="(i, idx) in pets" :key="idx" :pet="i" v-on:deletePet="deleteMe(idx)"></Pet>
+    </div>
+</template>
+
+<script>
+import Pet from './Pet.vue'
+export default {
+    data(){
+        return {
+            pets: [ 
+                {
+                    name: 'Pluto',
+                    type: 'dog'
+                },
+                {
+                    name: 'Cat',
+                    type: 'cat'
+                },
+                 {
+                    name: 'Cat',
+                    type: 'cat'
+                }
+            ]
+        }
+    },
+    components: {
+        Pet
+    },
+    methods: {
+        deleteMe(idx){
+            this.pets.splice(idx, 1)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
